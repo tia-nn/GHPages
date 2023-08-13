49d14d5c3c82d3b6010519c0a4fdc229b52418f9
tia-nn <yknms3533642@yahoo.co.jp>
2023-08-14 04:07:29 +0900

    build pdf-writer

---
+++ all.sh
@@ -0,0 +1,11 @@
+git submodule update --init
+
+# PDF-Writer
+cd PDF-Writer
+npm install
+cd src/components/writer/language/parser/antlr/
+antlr4 -Dlanguage=TypeScript -visitor -o dist PDFLexer.g4 PDFParser.g4
+cd -
+npm run build
+cd ..
+cp -r PDF-Writer/dist/pdf-writer/ dist/
