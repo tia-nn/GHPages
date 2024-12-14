6fa9799ea71895305147c60cc79cb40b06aca9c6
tia-nn <yknms3533642@yahoo.co.jp>
2024-12-15 03:28:19 +0900

    timeline 調整

--- src/sections/Timeline.tsx
+++ src/sections/Timeline.tsx
@@ -16,19 +16,45 @@ export default function Timeline() {
             <div className="h-12"></div>
 
             <div className="space-y-5">
-                <div className="relative">
-                    <div className="w-1/2 p-6 text-right bg-w">デンキヤギ株式会社でアルバイト</div>
-                    <span className="absolute w-6 h-6 top-0 bottom-0 left-0 right-0 m-auto bg-white rounded-full border-4 border-lavender"></span>
+
+                <div className="relative flex group">
+                    <div className="w-1/2 p-6 text-right text-xl content-center">2017/4</div>
+                    <div className="w-1/2 p-6 ml-auto break-keep [overflow-wrap:anywhere]">豊田高専<wbr /> 入学</div>
+                    <span className="
+                        absolute w-6 h-6 top-0 bottom-0 left-0 right-0 m-auto
+                        bg-white rounded-full border-4 border-lavender
+                        transition-all group-hover:border-8
+                    "></span>
                 </div>
 
-                <div className="relative">
-                    <div className="w-1/2 p-6 ml-auto">豊田高専卒業</div>
-                    <span className="absolute w-6 h-6 top-0 bottom-0 left-0 right-0 m-auto bg-white rounded-full border-4 border-lavender"></span>
+                <div className="relative flex group">
+                    <div className="w-1/2 p-6 text-right break-keep [overflow-wrap:anywhere]">デンキヤギ<wbr />で<wbr />アルバイト</div>
+                    <div className="w-1/2 p-6 ml-auto text-xl content-center">2019/4</div>
+                    <span className="
+                        absolute w-6 h-6 top-0 bottom-0 left-0 right-0 m-auto
+                        bg-white rounded-full border-4 border-lavender
+                        transition-all group-hover:border-8
+                    "></span>
                 </div>
 
-                <div className="relative">
-                    <div className="w-1/2 p-6 text-right">フラー株式会社に入社</div>
-                    <span className="absolute w-6 h-6 top-0 bottom-0 left-0 right-0 m-auto bg-white rounded-full border-4 border-lavender"></span>
+                <div className="relative flex group">
+                    <div className="w-1/2 p-6 text-right text-xl content-center">2023/3</div>
+                    <div className="w-1/2 p-6 ml-auto break-keep [overflow-wrap:anywhere]">豊田高専<wbr /> 卒業</div>
+                    <span className="
+                        absolute w-6 h-6 top-0 bottom-0 left-0 right-0 m-auto
+                        bg-white rounded-full border-4 border-lavender
+                        transition-all group-hover:border-8
+                    "></span>
+                </div>
+
+                <div className="relative flex group">
+                    <div className="w-1/2 p-6 text-right break-keep [overflow-wrap:anywhere]">フラー<wbr />に<wbr />入社</div>
+                    <div className="w-1/2 p-6 ml-auto text-xl content-center">2023/4</div>
+                    <span className="
+                        absolute w-6 h-6 top-0 bottom-0 left-0 right-0 m-auto
+                        bg-white rounded-full border-4 border-lavender
+                        transition-all group-hover:border-8
+                    "></span>
                 </div>
             </div>
 
